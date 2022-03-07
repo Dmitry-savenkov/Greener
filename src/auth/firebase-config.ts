4e50dd718cd62ae15cdd8fc6d7ea88ab5957b13e
Dmitry <dmitry2002savenkov@mail.ru>
2022-03-07 16:53:58 +0300

    greener-[be]-add signIn and login with firebase

---
+++ src/auth/firebase-config.ts
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAc2Jo6fsf6yQN3barCyLRaSrnMByQegpA',
+    authDomain: 'greener-7a3a1.firebaseapp.com',
+    projectId: 'greener-7a3a1',
+    storageBucket: 'greener-7a3a1.appspot.com',
+    messagingSenderId: '649597953455',
+    appId: '1:649597953455:web:347f07dc88dab6cc9a7b5a'
+};
