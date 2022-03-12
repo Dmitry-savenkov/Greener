ff3c9ea3fd10731ff8dbb1db236845e7497e708f
Dmitry <dmitry2002savenkov@mail.ru>
2022-03-12 23:48:12 +0300

    greener-[fe]-update layout

--- src/screens/ExploreScreen.tsx
+++ src/screens/ExploreScreen.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { LinearGradient } from 'expo-linear-gradient';
 import ExploreScreenData from '../data/ExploreScreenData';
-import { width, height } from '../constants/theme';
+import { width, height, colors } from '../constants/theme';
 import BackIcon from '../components/BackIcon';
+import SearchIcon from '../components/SearchIcon';
+
 const ExploreScreen = ({ navigation }) => {
     const [fontsLoaded] = useFonts({
         'SFUIDisplay-Regular': require('./../assets/fonts/SFUIDisplay-Regular.ttf'),
@@ -24,9 +26,33 @@ const ExploreScreen = ({ navigation }) => {
             >
                 <BackIcon />
             </TouchableOpacity>
-            <View>
-                <Text style={[styles.sectionTitle]}>Login</Text>
-            </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={[styles.header]}>
+                    <Text style={[styles.sectionTitle]}>Explore</Text>
+                    <View style={[styles.textInputBlock]}>
+                        <TextInput placeholder="Search" style={[styles.textInput]} />
+                        <TouchableOpacity>
+                            <SearchIcon />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View>
+                    <Image source={ExploreScreenData[0]} style={{ width: '100%', borderRadius: 10 }} />
+                </View>
+                <View style={[styles.rowStyle]}>
+                    <Image source={ExploreScreenData[1]} style={{ width: '66%', borderRadius: 10 }} />
+
+                    <Image source={ExploreScreenData[2]} style={{ width: '30%', borderRadius: 10 }} />
+                </View>
+                <View style={[styles.rowStyle]}>
+                    <Image source={ExploreScreenData[3]} style={{ width: '48%', borderRadius: 10 }} />
+
+                    <Image source={ExploreScreenData[4]} style={{ width: '48%', borderRadius: 10 }} />
+                </View>
+                <View style={[styles.bottomImage]}>
+                    <Image source={ExploreScreenData[5]} style={{ width: '100%', borderRadius: 10 }} />
+                </View>
+            </ScrollView>
         </View>
     );
 };
@@ -34,14 +60,45 @@ const ExploreScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ffffff',
+        backgroundColor: colors.white,
         paddingTop: height * 0.1,
         paddingHorizontal: width * 0.085
     },
     sectionTitle: {
-        marginTop: 35,
         fontSize: 26,
         fontFamily: 'SFUIDisplay-Medium'
+    },
+    header: {
+        marginBottom: 30,
+        marginTop: 35,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    textInputBlock: {
+        position: 'relative',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: 140,
+        height: 32,
+        backgroundColor: 'rgba(142, 142, 147, 0.06)',
+        borderRadius: 10
+    },
+    textInput: {
+        width: 100,
+        paddingLeft: 12,
+        paddingVertical: 8,
+        marginRight: 9
+    },
+    rowStyle: {
+        marginTop: 25,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    bottomImage: {
+        marginTop: 25
     }
 });
 
