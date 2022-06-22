fd1357594fccfe4425d5a5e600d99f8a8b871dae
Dmitry <dmitry2002savenkov@mail.ru>
2022-06-23 02:26:42 +0300

    greener-[fe]-implemented redux-saga and configurated persistConfig

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,19 +1,28 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 import { persistStore, persistReducer } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import mainSaga from '../sagas';
 
 const persistConfig = {
     key: 'root',
-    storage: AsyncStorage
+    storage: AsyncStorage,
+    version: 1,
+    whitelist: []
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const configureStore = () => createStore(persistedReducer, {});
+const sagaMiddleware = createSagaMiddleware();
+
+const configureStore = () => createStore(persistedReducer, {}, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 const configuredStore = configureStore();
 const persist = persistStore(configuredStore);
+
+sagaMiddleware.run(mainSaga);
 
 export { persist };
 export default configuredStore;
