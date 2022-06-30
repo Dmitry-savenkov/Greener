df0b4b8ccb03dcc39f0ce41222abb925de68234c
Dmitry <dmitry2002savenkov@mail.ru>
2022-06-30 03:08:23 +0300

    greener-[fe]-update styles sctructure of home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -24,12 +24,6 @@ import { width, height, colors } from '../constants/theme';
 import { ThemesContext } from '../context/ThemeContext';
 
 const HomeScreen = ({ navigation }) => {
-  const { homeScreenData, browseNameCategories, avatar } = useSelector((state) => ({
-    homeScreenData: state?.Browse.homeScreenData,
-    browseNameCategories: state?.Browse.browseNameCategories,
-    avatar: state?.User.avatar,
-  }));
-
   const flatlistRef = useRef(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -58,74 +52,51 @@ const HomeScreen = ({ navigation }) => {
             placeholder="Search"
             value={inputValue}
             onChangeText={(text) => setInputValue(text)}
-            style={{ marginRight: 15, width: '85%' }}
+            style={[styles.searchFormInput]}
           />
         </View>
         <TouchableOpacity>
           <NotificationsIcon size={30} color={colors.gray3} />
         </TouchableOpacity>
       </View>
-
-      <View>
-        <FlatList
-          ref={flatlistRef}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          data={[1, 2, 3, 4]}
-          decelerationRate="fast"
-          bounces={false}
-          onMomentumScrollEnd={(ev) => {
-            setActiveIndex(Math.round(ev.nativeEvent.contentOffset.x / width));
-          }}
-          keyExtractor={(item, index) => {
-            return item.id + index.toString();
-          }}
-          renderItem={({ item }) => {
-            return (
-              <MotiView style={{ width: width, overflow: 'hidden' }}>
-                <View
-                  style={{
-                    width: width - width * 0.08 * 2,
-                    height: 100,
-                    backgroundColor: '#CCF1BE',
-                    marginTop: 40,
-                    borderRadius: 15,
-                  }}
-                >
-                  <View style={{ marginLeft: 12, marginTop: 13 }}>
-                    <View
-                      style={{
-                        width: 100,
-                        height: 23,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        flexDirection: 'row',
-                        backgroundColor: colors.white,
-                        borderRadius: 10,
-                      }}
-                    >
-                      <Image source={require('../assets/twemoji_potted-plant.png')} />
-                      <View>
-                        <Text style={{ color: '#5A5A5A', marginLeft: 5 }}>Plant tips</Text>
-                      </View>
-                    </View>
-                    <View style={{ marginTop: 10, width: 120 }}>
-                      <Text style={{ color: '#5A5A5A', fontWeight: '500', fontSize: 18 }}>
-                        Plants make people happy!
-                      </Text>
+      <FlatList
+        ref={flatlistRef}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        data={[1, 2, 3, 4]}
+        decelerationRate="fast"
+        bounces={false}
+        onMomentumScrollEnd={(ev) => {
+          setActiveIndex(Math.round(ev.nativeEvent.contentOffset.x / width));
+        }}
+        keyExtractor={(item, index) => {
+          return item.id + index.toString();
+        }}
+        renderItem={({ item }) => {
+          return (
+            <MotiView style={[styles.sliderContainer]}>
+              <View style={styles.sliderBackground}>
+                <View style={[styles.sliderInfoWrapper]}>
+                  <View style={[styles.sliderLabel]}>
+                    <Image source={require('../assets/twemoji_potted-plant.png')} />
+                    <View>
+                      <Text style={[styles.sliderLabelText]}>Plant tips</Text>
                     </View>
                   </View>
+                  <View style={[styles.sliderTitleWrapper]}>
+                    <Text style={[styles.sliderTitleText]}>Plants make people happy!</Text>
+                  </View>
                 </View>
-                <Image
-                  source={require('../assets/unsplash_XXpbdU_31Sg.png')}
-                  style={{ position: 'absolute', right: width * 0.08 * 2, bottom: 0 }}
-                />
-              </MotiView>
-            );
-          }}
-        />
-      </View>
-      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 18 }}>
+              </View>
+              <Image
+                source={require('../assets/unsplash_XXpbdU_31Sg.png')}
+                style={[styles.sliderOverflowImage]}
+              />
+            </MotiView>
+          );
+        }}
+      />
+      <View style={[styles.dotsContainer]}>
         <View
           style={{
             width: 50,
@@ -146,7 +117,6 @@ const HomeScreen = ({ navigation }) => {
                   style={[
                     {
                       width: activeIndex === index ? 7 : 5,
-
                       height: activeIndex === index ? 7 : 5,
                       backgroundColor: activeIndex === index ? '#60BF96' : '#CCF1BE',
                       borderRadius: 10,
@@ -181,66 +151,73 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 16,
     backgroundColor: colors.white,
-  },
-  listCategory: {
-    marginTop: 50,
-  },
-  categoryNameItem: {
-    marginRight: 30,
-  },
-  categoryNameText: {
-    fontFamily: 'SFUIDisplay-Medium',
-    fontSize: 18,
-  },
-  grayLine: {
-    width: '100%',
-    height: 1,
-    backgroundColor: 'rgba(225, 227, 232, 1)',
-    marginBottom: 40,
-  },
-  underlineGreen: {
-    marginTop: 21,
-    width: '100%',
-    height: 3,
-    backgroundColor: colors.primary,
-  },
-  categoryBlock: {
-    alignItems: 'center',
-    width: '45%',
-    height: 150,
-    margin: 7.5,
-    backgroundColor: colors.white,
-    shadowColor: '#171717',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.02,
-    shadowRadius: 10,
-  },
-  shadowFlatList: {
-    shadowColor: '#171717',
-    shadowOffset: { width: 0, height: 0 },
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.08,
-    shadowRadius: 10,
+    shadowRadius: 5,
   },
-  imageBG: {
-    marginTop: 25,
+  searchFormInput: {
+    marginRight: 15,
+    width: '85%',
+  },
+  sliderContainer: {
+    width: width,
+    overflow: 'hidden',
+  },
+  sliderBackground: {
+    width: width - width * 0.08 * 2,
+    height: 100,
+    backgroundColor: '#CCF1BE',
+    marginTop: 40,
+    borderRadius: 15,
+  },
+  sliderLabel: {
+    width: 100,
+    height: 23,
     alignItems: 'center',
     justifyContent: 'center',
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    backgroundColor: 'rgba(43, 218, 142, 0.2)',
-    marginBottom: 15,
+    flexDirection: 'row',
+    backgroundColor: colors.white,
+    borderRadius: 10,
   },
-  categoryBlockTitle: {
-    fontSize: 14,
-    color: 'black',
-    fontFamily: 'SFUIDisplay-Medium',
-    marginBottom: 4,
+  sliderLabelText: {
+    color: '#5A5A5A',
+    marginLeft: 5,
   },
-  categoryBlockCount: {
-    color: colors.gray2,
-    fontFamily: 'SFUIDisplay-Regular',
+  sliderInfoWrapper: {
+    marginLeft: 12,
+    marginTop: 13,
   },
+  sliderTitleWrapper: {
+    marginTop: 10,
+    width: 120,
+  },
+  sliderTitleText: {
+    color: '#5A5A5A',
+    fontWeight: '500',
+    fontSize: 18,
+  },
+  sliderOverflowImage: {
+    position: 'absolute',
+    right: width * 0.08 * 2,
+    bottom: 0,
+  },
+  dotsContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 18,
+  },
+  // categoryBlock: {
+  //   alignItems: 'center',
+  //   width: '45%',
+  //   height: 150,
+  //   margin: 7.5,
+  //   backgroundColor: colors.white,
+  // shadowColor: '#171717',
+  // shadowOffset: { width: 0, height: 0 },
+  // shadowOpacity: 0.02,
+  // shadowRadius: 10,
+  // },
 });
 
 export default HomeScreen;
