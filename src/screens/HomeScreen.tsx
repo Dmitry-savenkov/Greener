579849e76977f29487a98100dba43172dd0b9a02
Dmitry <dmitry2002savenkov@mail.ru>
2022-07-01 01:51:32 +0300

    greener-[fe]-add best plants component

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -151,6 +151,43 @@ const HomeScreen = ({ navigation }) => {
           </TouchableOpacity>
         )}
       />
+      <View style={[styles.blockTitleWrapper]}>
+        <Text style={[styles.blockTitle]}>Best Plants</Text>
+      </View>
+      <View>
+        <FlatList
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          data={[1, 2, 3]}
+          decelerationRate="fast"
+          bounces={false}
+          keyExtractor={(item, index) => {
+            return item.id + index.toString();
+          }}
+          renderItem={() => (
+            <TouchableOpacity>
+              <View style={[styles.bestPlantsItem]}>
+                <View>
+                  <View style={[styles.bestPlantsLabel]}>
+                    <Text style={[styles.bestPlantsLabelText]}>Best Seller</Text>
+                  </View>
+                  <Image
+                    source={require('../assets/photoPlant.png')}
+                    style={[styles.bestPlantsImage]}
+                  />
+                </View>
+                <View>
+                  <Text style={[styles.bestPlantsTitle]}>Monstera Delisiosa</Text>
+                  <Text style={[styles.bestPlantsPrice]}>40-90$</Text>
+                  <View style={[styles.bestPlantsLinkWrapper]}>
+                    <Text style={[styles.bestPlantsLinkText]}>Show More</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </ScrollView>
   );
 };
@@ -257,7 +294,66 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  categoriesTitle: { fontSize: 14, fontWeight: '400', color: '#5A5A5A', marginTop: 5 },
+  categoriesTitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#5A5A5A',
+    marginTop: 5,
+  },
+  bestPlantsItem: {
+    marginTop: 10,
+    marginRight: 23,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: 150,
+    height: 230,
+  },
+  bestPlantsLabel: {
+    position: 'absolute',
+    left: 0,
+    top: 20,
+    padding: 4,
+    borderTopRightRadius: 6,
+    borderBottomRightRadius: 6,
+    zIndex: 10,
+    backgroundColor: 'white',
+  },
+  bestPlantsLabelText: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: '#5A5A5A',
+  },
+  bestPlantsImage: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    width: 150,
+    height: 155,
+    resizeMode: 'cover',
+  },
+  bestPlantsTitle: {
+    marginTop: 8,
+    marginLeft: 12,
+    fontSize: 12,
+    fontWeight: '400',
+    color: '#5A5A5A',
+  },
+  bestPlantsPrice: {
+    marginTop: 5,
+    marginLeft: 12,
+    fontSize: 12,
+    fontWeight: '400',
+    color: '#5A5A5A',
+  },
+  bestPlantsLinkWrapper: {
+    alignItems: 'flex-end',
+    marginRight: 8,
+  },
+  bestPlantsLinkText: {
+    textDecorationLine: 'underline',
+    fontSize: 12,
+    fontWeight: '500',
+    color: '#5A5A5A',
+  },
   // categoryBlock: {
   //   alignItems: 'center',
   //   width: '45%',
