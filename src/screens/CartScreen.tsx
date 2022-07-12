e7b4a6b8149664c0fcb4c7774165ad1f8b07cd34
Dmitry <dmitry2002savenkov@mail.ru>
2022-07-12 20:59:49 +0300

    greener-[fe]-delete added item from card using swipe

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,5 +1,5 @@
 // Lib
-import React, { useState, useMemo, useContext } from 'react';
+import React, { useState, useRef, useCallback, useContext } from 'react';
 import {
   StyleSheet,
   Text,
@@ -14,36 +14,42 @@ import { useSelector } from 'react-redux';
 
 // Components
 import BackIcon from '../components/icons/BackIcon';
-import DotsIcon from '../components/icons/DotsIcon';
+import CartItem from '../components/CartItem';
 
 // UI
 import { width, height, colors } from '../constants/theme';
 import { ThemesContext } from '../context/ThemeContext';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+const TITLES = [
+  'Record the dismissible tutorial 🎥',
+  'Leave 👍🏼 to the video',
+  'Check YouTube comments',
+  'Subscribe to the channel 🚀',
+  'Leave a ⭐️ on the GitHub Repo',
+];
+
+const TASKS = TITLES.map((title, index) => ({ title, index }));
 
 const CartScreen = ({ navigation }) => {
   const { items } = useSelector((state: any) => ({
     items: state?.Cart?.items,
   }));
+  const [tasks, setTasks] = useState(TASKS);
+
+  const onDismiss = useCallback((task) => {}, []);
 
   const { fontsLoaded } = useContext(ThemesContext);
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+  const scrollRef = useRef(null);
+
   return (
-    <ScrollView style={[styles.container]}>
-      <View>
-        {items.map((item, index) => {
-          return (
-            <View
-              key={item.title + index}
-              style={{ width: '100%', height: 100, alignItems: 'center', justifyContent: 'center' }}
-            >
-              <Text style={{ color: 'black' }}>{item.label}</Text>
-            </View>
-          );
-        })}
-      </View>
+    <ScrollView style={[styles.container]} ref={scrollRef}>
+      {items.map((task) => (
+        <CartItem simultaneousHandlers={scrollRef} key={task.index} task={task} />
+      ))}
     </ScrollView>
   );
 };
