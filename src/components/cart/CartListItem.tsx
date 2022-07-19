6154a4515eaeedbd6352b6544d55458254a53f15
Dmitry <dmitry2002savenkov@mail.ru>
2022-07-20 00:59:13 +0300

    greener-[fe]-updated order list item layout

--- src/components/cart/CartListItem.tsx
+++ src/components/cart/CartListItem.tsx
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 
 // Actions
 import { DecreasingNumberItemsCart, IncreasingNumberItemsCart } from '../../redux/actions/cart';
+import EntypoIcon from '../icons/EntypoIcon';
 
 const CartListItem = ({ navigation, plant, activeImageObject, count }) => {
   const { color, image, planter, size } = activeImageObject;
@@ -20,35 +21,74 @@ const CartListItem = ({ navigation, plant, activeImageObject, count }) => {
       >
         <Image source={image} style={{ width: 80, height: 100, borderRadius: 8 }} />
       </TouchableOpacity>
-      <View>
-        <Text>{plant.title}</Text>
-        <Text>{count}</Text>
-        <Text>color: {color}</Text>
-        <Text>planter type: {planter}</Text>
-        <Text>planter size: {size}</Text>
-        <TouchableOpacity
-          onPress={() => {
-            dispatch(
-              IncreasingNumberItemsCart({
-                id: plant.id,
-              }),
-            );
+      <View style={{ marginLeft: 15, marginTop: 6 }}>
+        <Text style={{ fontSize: 16, color: '#2C2C2C', fontWeight: '400' }}>{plant.title}</Text>
+        <View style={{ flexDirection: 'row', marginTop: 6 }}>
+          <Text style={{ fontSize: 14, color: '#2C2C2C', fontWeight: '400' }}>{size} / </Text>
+          <Text style={{ fontSize: 14, color: '#2C2C2C', fontWeight: '400' }}>{color} / </Text>
+          <Text style={{ fontSize: 14, color: '#2C2C2C', fontWeight: '400' }}>{planter} </Text>
+        </View>
+        <View style={{ marginTop: 6 }}>
+          <Text style={{ fontSize: 14, color: '#2C2C2C', fontWeight: '400' }}>55$</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            width: 80,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 10,
           }}
         >
-          <Text>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          disabled={count === 1}
-          onPress={() => {
-            dispatch(
-              DecreasingNumberItemsCart({
-                id: plant.id,
-              }),
-            );
-          }}
-        >
-          <Text>-</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            disabled={count === 1}
+            onPress={() => {
+              dispatch(
+                DecreasingNumberItemsCart({
+                  id: plant.id,
+                }),
+              );
+            }}
+          >
+            <View
+              style={{
+                width: 18,
+                height: 18,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 16,
+                borderWidth: 1,
+                borderColor: count === 1 ? '#848484' : '#2C2C2C',
+              }}
+            >
+              <EntypoIcon name="minus" size={14} color={count === 1 ? '#848484' : '#2C2C2C'} />
+            </View>
+          </TouchableOpacity>
+          <Text style={{ fontSize: 16, color: '#2C2C2C', fontWeight: '400' }}>{count}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              dispatch(
+                IncreasingNumberItemsCart({
+                  id: plant.id,
+                }),
+              );
+            }}
+          >
+            <View
+              style={{
+                width: 18,
+                height: 18,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 16,
+                borderWidth: 1,
+                borderColor: '#2C2C2C',
+              }}
+            >
+              <EntypoIcon name="plus" size={14} color="#2C2C2C" />
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </Fragment>
   );
