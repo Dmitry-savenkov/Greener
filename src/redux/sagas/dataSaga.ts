408a318ae37f6070f24a323860f8ded52cf1ccdb
Dmitry <dmitry2002savenkov@mail.ru>
2022-08-05 02:40:14 +0300

    greener-[fe]-impemented react-native-toast-notifications

--- src/redux/sagas/dataSaga.ts
+++ src/redux/sagas/dataSaga.ts
@@ -4,7 +4,14 @@ import { takeEvery, put, select } from 'redux-saga/effects';
 // Actions
 import { AddItemToCard } from '../actions/cart';
 
-export function* userDataLoadSaga({ payload }) {}
+export function* userDataLoadSaga({ payload }) {
+  toast.show('Task finished successfully', {
+    type: 'success',
+    placement: 'top',
+    duration: 4000,
+    animationType: 'slide-in',
+  });
+}
 
 function* dataSaga() {
   yield takeEvery(AddItemToCard, userDataLoadSaga);
