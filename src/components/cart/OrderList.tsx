ab4539425a1f37ab9a4d088a2a7c3c6ea2c30e82
Dmitry <dmitry2002savenkov@mail.ru>
2022-08-15 23:30:02 +0300

    greener-[fe]-implemented lottie animations

--- src/components/cart/OrderList.tsx
+++ src/components/cart/OrderList.tsx
@@ -6,12 +6,16 @@ import { useDispatch } from 'react-redux';
 
 // Components
 import CartList from './CartList';
+import LottieAnimation from '../LottieAnimation';
 
 // Actions
 import { DeleteItemFromCard } from '../../redux/actions/cart';
 
 // Selectors
 import { getOrderListItems } from '../../redux/selectors';
+
+// Animation
+import emptyBox from '../../assets/lottie/empty-box.json';
 
 const OrderList = ({ navigation }) => {
   const items = useSelector(getOrderListItems);
@@ -29,16 +33,28 @@ const OrderList = ({ navigation }) => {
     () => (
       <View style={[styles.container]}>
         <Text style={[styles.title]}>Order List</Text>
-        {items.map(({ plant, activeImageObject, count }) => (
-          <CartList
-            key={plant.title + Math.random()}
-            onDismiss={onDismiss}
-            plant={plant}
-            count={count}
-            activeImageObject={activeImageObject}
-            navigation={navigation}
-          />
-        ))}
+        {items.length !== 0 ? (
+          items.map(({ plant, activeImageObject, count }) => (
+            <CartList
+              key={plant.title + Math.random()}
+              onDismiss={onDismiss}
+              plant={plant}
+              count={count}
+              activeImageObject={activeImageObject}
+              navigation={navigation}
+            />
+          ))
+        ) : (
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <LottieAnimation
+              width={100}
+              height={200}
+              lottieJSON={emptyBox}
+              autoPlay={true}
+              loop={true}
+            />
+          </View>
+        )}
       </View>
     ),
     [items, navigation, onDismiss],
